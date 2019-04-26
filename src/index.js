f1f54fddf580ba41b5b9f4adb78908801db54767
Mehren <stefan.mehren@schott-music.com>
2019-04-26 17:14:44 +0200

    Next Commit

--- src/index.js
+++ src/index.js
@@ -2,43 +2,68 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Tweet() {
+const testTweet = {
+  message: "Blabliblablub.",
+  gravatar: "xyz",
+  author: {
+    handle: "catperson",
+    name: "IAMA Cat Person"
+  },
+  likes: 2,
+  retweets: 0,
+  timestamp: "2016-07-30 21:24:37"
+}
+
+function Tweet({ tweet }) {
   return (
       <div className="tweet">
-      <Avatar/>
+      <Avatar hash={tweet.gravatar}/>
         <div className="content">
-          <NameWithHandle/>
-          <Message/>
+          <NameWithHandle author={tweet.author}/>
+          <Message text={tweet.message}/>
+          <Time time={tweet.timestamp}/>
+          <div className="buttons">
+
+          </div>
         </div>
       </div>
   );
 }
 
-function Avatar() {
+function Avatar({ hash }) {
+  const url = `https://www.gravatar.com/avatar/${hash}`;
   return(
-    <img src="https://www.gravatar.com/avatar/nothing" className="avatar" alt="avatar" />
+    <img src={url} className="avatar" alt="avatar" />
   );
 }
 
-function Message() {
+function Message({ text }) {
   return (
-    <div className="Message">
-      This is less than 140 characters.
+    <div className="message">
+      {text}
     </div>
   );
 }
 
-function NameWithHandle() {
+function NameWithHandle({ author }) {
   return (
     <span className="name-with-handle">
       <span className="name">
-        Your Name
+        {author.name}
       </span>
       <span className="handle">
-        @yourhandle
+        @{author.handle}
       </span>
     </span>
   )
 }
 
-ReactDOM.render(<Tweet/>, document.querySelector('#root'));
+function Time({ time }) {
+  return (
+    <div>
+      { time }
+    </div>
+  )
+}
+
+ReactDOM.render(<Tweet tweet={testTweet}/>, document.querySelector('#root'));
